2b1db2bba4cdefe27926225cd04e5adfc28b7ff9
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-25 16:39:17 +0500

    working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 
 
 function Navbar() {
@@ -6,8 +7,8 @@ function Navbar() {
   return (
     <>
     <div className="d-flex justify-content-center gap-5 m-5">
-    <h6>All products</h6>
-    <h6>Single Products {selector.length}</h6>
+        <h6><Link to="">All Products</Link></h6>
+        <h6><Link to="SingleProduct">Single Product {selector.length}</Link></h6>
     </div>
     </>
   )
---
+++ src/screens/SingleProduct.jsx
@@ -0,0 +1,11 @@
+
+
+const SingleProduct = () => {
+  return (
+    <>
+    <h1>single product</h1>
+    </>
+  )
+}
+
+export default SingleProduct
