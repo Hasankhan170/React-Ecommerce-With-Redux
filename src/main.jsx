d677a97e58a34890e1218287ab9553197bb1b333
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-25 16:44:17 +0500

    working

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,27 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import SingleProduct from './screens/SingleProduct.jsx';
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <App/>,
+    children : [
+      {
+        path : 'SingleProduct',
+        element : <SingleProduct/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <RouterProvider router={router}> 
     <App />
+    </RouterProvider>
   </Provider>
 
 )
