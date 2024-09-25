e9b5d5de4831ec8a651c547cc14ff23a44b27d63
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-25 16:52:26 +0500

    working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import "../components/Navbar.css"
 
 
 function Navbar() {
@@ -7,8 +8,18 @@ function Navbar() {
   return (
     <>
     <div className="d-flex justify-content-center gap-5 m-5">
-        <h6><Link to="">All Products</Link></h6>
-        <h6><Link to="SingleProduct">Single Product {selector.length}</Link></h6>
+        <h6><Link style={{textDecoration :'none'}} to="">All Products</Link></h6>
+        <h6>
+  <Link style={{ textDecoration: 'none', position: 'relative' }} to="SingleProduct">
+    Single Product
+    <span style={{ position: 'relative' }}>
+      <i className="fa-solid fa-cart-plus"></i>
+      {selector.length > 0 && (
+        <span className="cart-count">{selector.length}</span>
+      )}
+    </span>
+  </Link>
+</h6>
     </div>
     </>
   )
